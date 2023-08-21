c2713d6b312de64c081277e92f5c9d7f52e7b966
Sezer Ataş <sezeratas@gmail.com>
2023-08-21 17:51:35 +0300

    RepositoryLayerFinal

--- Repositories/EFCore/RepositoryBase.cs
+++ Repositories/EFCore/RepositoryBase.cs
@@ -1,4 +1,5 @@
-﻿using Repositories.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,32 +9,31 @@ using System.Threading.Tasks;
 
 namespace Repositories.EFCore
 {
-    public class RepositoryBase<T> : IRepositoryBase<T>
+    public abstract class RepositoryBase<T> : IRepositoryBase<T>
         where T : class
     {
-        public void Create(T entity)
+        protected readonly RepositoryContext _context;
+
+        public RepositoryBase(RepositoryContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public void Delete(T entity)
-        {
-            throw new NotImplementedException();
-        }
+        public void Create(T entity) => _context.Set<T>().Add(entity);
 
-        public IQueryable<T> FindAll(bool trackChanges)
-        {
-            throw new NotImplementedException();
-        }
+        public void Delete(T entity) => _context?.Set<T>().Remove(entity);
 
-        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
-        {
-            throw new NotImplementedException();
-        }
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ?
+            _context.Set<T>().AsNoTracking() :
+            _context.Set<T>();
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression,
+            bool trackChanges) =>
+            !trackChanges ?
+            _context.Set<T>().Where(expression).AsNoTracking() :
+            _context.Set<T>().Where(expression);
 
-        public void Update(T entity)
-        {
-            throw new NotImplementedException();
-        }
+
+        public void Update(T entity) => _context.Set<T>().Update(entity);
     }
 }
