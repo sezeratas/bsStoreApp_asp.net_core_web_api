fc37f5cf623f2d043a31b7611d1fe0b895714cb5
Sezer Ataş <sezeratas@gmail.com>
2023-08-23 14:29:09 +0300

    NLogExtensionsLogging

---
+++ Services/Contract/ILoggerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contract
+{
+    public interface ILoggerService
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+        void LogDebug(string message);
+
+    }
+}
