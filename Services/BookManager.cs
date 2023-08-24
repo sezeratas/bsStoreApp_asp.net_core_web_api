06873cce7b181aac79dc82f6972da17505295f44
Sezer Ataş <sezeratas@gmail.com>
2023-08-24 13:59:14 +0300

    GlobalErrorFinal

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Entities.Exceptions;
+using Entities.Models;
 using Repositories.Contracts;
 using Services.Contract;
 using System;
@@ -32,9 +33,7 @@ namespace Services
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
             {
-                string message = $"The book with id:{id} could not found.";
-                _logger.LogInfo(message);
-                throw new Exception(message);
+                throw new BookNotFoundException(id);
             }
 
             _manager.Book.DeleteOneBook(entity);
@@ -46,18 +45,17 @@ namespace Services
         }
         public Book GetOneBookById(int id, bool trackChanges) 
         { 
-            return _manager.Book.GetOneBookById(id, trackChanges);
+            var book = _manager.Book.GetOneBookById(id, trackChanges);
+            if (book is null)
+                throw new BookNotFoundException(id);
+            return book;
         }
         public void UpdateOneBook(int id, Book book, bool trackChanges)
         {
             //check entity
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
-            {
-                string msg = $"Book with id: {id} could not found.";
-                _logger.LogInfo(msg);
-                throw new Exception(msg);
-            }
+                throw new BookNotFoundException (id);
 
             // check params
             if(book is null)
