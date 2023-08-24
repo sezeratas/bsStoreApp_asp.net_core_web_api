418ca78168bc64e770f22aa223917f6ade19e6a2
Sezer Ataş <sezeratas@gmail.com>
2023-08-24 09:50:36 +0300

    UseExceptionHandler

---
+++ WebApi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Entities.ErrorModel;
+using Microsoft.AspNetCore.Diagnostics;
+using Services.Contract;
+using System.Net;
+
+namespace WebApi.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app,
+            ILoggerService logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if(contextFeature is not null)
+                    {
+                        logger.LogError($"Something went wrong: {contextFeature}");
+                        await context.Response.WriteAsync(new ErrorDetails() { 
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error"
+                        }.ToString());
+                    }
+                });
+            });
+
+        }
+    }
+}
